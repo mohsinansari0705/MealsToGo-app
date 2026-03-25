f9826ee672b3dfb524fd25cef85d2df2686b2b2d
Mohsin Ansari <mohsinansari0705@gmail.com>
2026-03-25 14:08:13 +0530

    [client] ThemeProvider wrapper added for theming

--- client/App.tsx
+++ client/App.tsx
@@ -1,16 +1,19 @@
-import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import { RestaurantsScreen } from "./app/restaurants/restaurants.screen";
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { RestaurantsScreen } from './app/restaurants/restaurants.screen';
+import { ThemeProvider } from './theme/ThemeContext';
 
 
 export default function App() {
   return (
     <>
-      <StatusBar style="auto" animated={true} />
+      <StatusBar style='auto' animated={true} />
 
       <SafeAreaProvider>
-        <RestaurantsScreen />
+        <ThemeProvider>
+          <RestaurantsScreen />
+        </ThemeProvider>
       </SafeAreaProvider>
     </>
   );
