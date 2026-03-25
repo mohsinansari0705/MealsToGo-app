1eae5d9783822ec09200955a061736b8629bc64f
Mohsin Ansari <mohsinansari0705@gmail.com>
2026-03-25 14:11:21 +0530

    [client/theme] typographt added with fontSize, fontWeight & lineHeight

---
+++ client/theme/typography.ts
@@ -0,0 +1,27 @@
+export const fonts = {
+  body: 'Oswald_400Regular',
+  heading: 'Lato_400Regular',
+  monospace: 'Oswald_400Regular',
+};
+
+export const fontSize = {
+  xs: 11,
+  sm: 13,
+  md: 15,
+  lg: 17,
+  xl: 20,
+  xxl: 24,
+  xxxl: 30,
+};
+
+export const fontWeight = {
+  regular: 400,
+  medium: 500,
+  bold: 700,
+};
+
+export const lineHeight = {
+  tight: 1.2,
+  normal: 1.5,
+  loose: 1.8,
+};
