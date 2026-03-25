96031f4925ace15ae31fde00143a36e06d722b0b
Mohsin Ansari <mohsinansari0705@gmail.com>
2026-03-25 14:12:30 +0530

    [client/prettier] formatted with prettier

--- client/components/RestaurantInfoCard.tsx
+++ client/components/RestaurantInfoCard.tsx
@@ -1,6 +1,7 @@
-import React from "react";
-import { View, Text } from "react-native";
-import { Card } from "react-native-paper";
+import React from 'react';
+import { View, Text } from 'react-native';
+import { Card } from 'react-native-paper';
+
 
 type Restaurant = {
   name: string;
@@ -15,22 +16,10 @@ type RestaurantInfoCardProps = {
   restaurants: Restaurant;
 };
 
-export const RestaurantInfoCard = ({
-  restaurants = {
-    name: "Some Restaurant",
-    icon: "random Icon",
-    photos: [
-      "https://imgs.search.brave.com/qLazr26Lu6vYYB-vHR6zUvWJh-KaXqczO7trkHnx8iY/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE2/MjU4MTM1MDYwNjIt/MGFlYjFkN2EwOTRi/P2ZtPWpwZyZxPTYw/Jnc9MzAwMCZhdXRv/PWZvcm1hdCZmaXQ9/Y3JvcCZpeGxpYj1y/Yi00LjEuMCZpeGlk/PU0zd3hNakEzZkRC/OE1IeHpaV0Z5WTJo/OE1URjhmR0oxY21k/bGNueGxibnd3Zkh3/d2ZIeDhNQT09",
-    ],
-    address: "100 some random street",
-    isOpenNow: true,
-    rating: 4,
-    isClosedTemporarily: false,
-  },
-}: RestaurantInfoCardProps) => {
+export const RestaurantInfoCard = ({ restaurants }: RestaurantInfoCardProps) => {
   return (
     <View>
-      <Card mode="contained">
+      <Card mode='contained'>
         <Card.Cover
           key={restaurants.name}
           source={{ uri: restaurants.photos[0] }}
