f108ec031b0708045952fdb6807b3349fd7a6026
Mohsin Ansari <mohsinansari0705@gmail.com>
2026-03-25 21:10:00 +0530

    [client] updated styling from theming

--- client/components/RestaurantInfoCard.tsx
+++ client/components/RestaurantInfoCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { Card } from 'react-native-paper';
+import { useTheme } from '../theme/ThemeContext';
 
 
 type Restaurant = {
@@ -17,16 +18,23 @@ type RestaurantInfoCardProps = {
 };
 
 export const RestaurantInfoCard = ({ restaurants }: RestaurantInfoCardProps) => {
+  const theme = useTheme().theme;
+
   return (
-    <View>
+    <View style={{ paddingHorizontal: theme.space.md }}>
       <Card mode='contained'>
         <Card.Cover
           key={restaurants.name}
           source={{ uri: restaurants.photos[0] }}
-          style={{ margin: 16 }}
+          style={{ margin: theme.space.md }}
         />
         <Card.Content>
-          <Text style={{ fontSize: 16 }}>{restaurants.name}</Text>
+          <Text
+            style={{
+              fontSize: theme.fontSize.md,
+              fontFamily: theme.fonts.heading
+            }}
+          >{restaurants.name}</Text>
         </Card.Content>
       </Card>
     </View>
