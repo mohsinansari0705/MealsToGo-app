41bcc2f54fa3fcaca1048a4e8c68099809d1d429
Mohsin Ansari <mohsinansari0705@gmail.com>
2026-03-28 23:25:07 +0530

    [client] build the restaurant info cards with closed or open icons

--- client/components/RestaurantInfoCard.tsx
+++ client/components/RestaurantInfoCard.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import { Card } from 'react-native-paper';
+import { SvgXml } from 'react-native-svg';
+import { Ionicons } from '@expo/vector-icons';
+import open from '../assets/icons/open';
 import { useTheme } from '../theme/ThemeContext';
 
 
@@ -20,6 +23,8 @@ type RestaurantInfoCardProps = {
 export const RestaurantInfoCard = ({ restaurants }: RestaurantInfoCardProps) => {
   const theme = useTheme().theme;
 
+  const ratingArray: any[] = Array.from(new Array(Math.floor(restaurants.rating)));
+
   return (
     <View style={{ paddingHorizontal: theme.space.md }}>
       <Card mode='contained'>
@@ -29,12 +34,31 @@ export const RestaurantInfoCard = ({ restaurants }: RestaurantInfoCardProps) => 
           style={{ margin: theme.space.md }}
         />
         <Card.Content>
-          <Text
-            style={{
-              fontSize: theme.fontSize.md,
-              fontFamily: theme.fonts.heading
-            }}
-          >{restaurants.name}</Text>
+          <Text style={{ fontFamily: theme.fonts.heading, fontSize: theme.fontSize.md * 1.1 }}>
+            {restaurants.name}
+          </Text>
+
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+            <View style={{ flexDirection: 'row', gap: theme.space.xs * 0.75, marginVertical: theme.space.xs }}>
+              {ratingArray.map(() =>
+                <Ionicons name='star' size={theme.space.md} color={theme.colors.warning} />
+              )}
+            </View>
+            
+            <View style={{ flexDirection: 'row', gap: theme.space.sm * 1.25, alignItems: 'center' }}>
+              {restaurants.isClosedTemporarily && (
+                <Text style={{ fontFamily: theme.fonts.heading, color: theme.colors.danger, fontSize: theme.fontSize.sm }}>CLOSED TEMPORARILY</Text>
+              )}
+
+              {restaurants.isOpenNow && <SvgXml xml={open} width={theme.space.md * 1.25} height={theme.space.md * 1.25} />}
+
+              <Image source={{ uri: restaurants.icon }} style={{ height: theme.space.md, width: theme.space.md }} />
+            </View>
+          </View>
+
+          <Text style={{ fontFamily: theme.fonts.body, fontSize: theme.fontSize.md }}>
+            {restaurants.address}
+          </Text>
         </Card.Content>
       </Card>
     </View>
