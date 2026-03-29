a751ad875f23dce9c918e6b8ba090a6e0aa6b64b
Mohsin Ansari <mohsinansari0705@gmail.com>
2026-03-29 22:44:59 +0530

    [client] Flatlist added to list all restaurant cards in Restaurant Screen

--- client/app/restaurants/restaurants.screen.tsx
+++ client/app/restaurants/restaurants.screen.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View } from 'react-native';
+import { View, FlatList } from 'react-native';
 import { Searchbar } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { RestaurantInfoCard } from '../../components/RestaurantInfoCard';
@@ -20,18 +20,11 @@ export const RestaurantsScreen = () => {
         />
       </View>
 
-      <RestaurantInfoCard
-        restaurants={{
-          name: 'Some Restaurant',
-          icon: 'random Icon',
-          photos: [
-            'https://imgs.search.brave.com/qLazr26Lu6vYYB-vHR6zUvWJh-KaXqczO7trkHnx8iY/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE2/MjU4MTM1MDYwNjIt/MGFlYjFkN2EwOTRi/P2ZtPWpwZyZxPTYw/Jnc9MzAwMCZhdXRv/PWZvcm1hdCZmaXQ9/Y3JvcCZpeGxpYj1y/Yi00LjEuMCZpeGlk/PU0zd3hNakEzZkRC/OE1IeHpaV0Z5WTJo/OE1URjhmR0oxY21k/bGNueGxibnd3Zkh3/d2ZIeDhNQT09',
-          ],
-          address: '100 some random street',
-          isOpenNow: true,
-          rating: 4,
-          isClosedTemporarily: false,
-        }}
+      <FlatList
+        data={[ {name: 1}, {name: 2}, {name: 3}, {name: 4}, {name: 5} ]}
+        renderItem={() => <RestaurantInfoCard />}
+        keyExtractor={(item) => item.name.toString()}
+        contentContainerStyle={{ paddingHorizontal: theme.space.md, gap: theme.space.md, marginBottom: theme.space.xl }}
       />
     </SafeAreaView>
   );
