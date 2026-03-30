36e9a9401ce7cbde30248a3cf60a5ca33d99f748
Mohsin Ansari <mohsinansari0705@gmail.com>
2026-03-30 23:21:38 +0530

    [client] navigation logic added with icons to app root

--- client/App.tsx
+++ client/App.tsx
@@ -1,9 +1,47 @@
 import React from 'react';
 import { useFonts } from 'expo-font';
+import { View, Text } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { RestaurantsScreen } from './app/restaurants/restaurants.screen';
+import { Ionicons } from '@expo/vector-icons';
 import { ThemeProvider } from './theme/ThemeContext';
+import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import { RestaurantsScreen } from './app/restaurants/restaurants.screen';
+
+
+const Tab = createBottomTabNavigator();
+const TAB_ICON: Record<string, keyof typeof Ionicons.glyphMap> = {
+  Restaurants: 'restaurant',
+  Map: 'map-sharp',
+  Settings: 'settings',
+};
+
+const Map = () => (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text>Map!</Text>
+  </View>
+);
+const Settings = () => (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text>Settings!</Text>
+  </View>
+);
+
+const tabScreenOptions = ({ route }: { route: any }) => {
+  let iconName = TAB_ICON[route.name];
+
+  return {
+    headerTransparent: true,
+    headerTitle: '',
+    tabBarIcon: ({ color, size }: { color: string; size: number }) => (
+      <Ionicons name={iconName} size={size} color={color} />
+    ),
+    tabBarActiveTintColor: 'tomato',
+    tabBarInactiveTintColor: 'gray',
+  };
+};
 
 
 export default function App() {
@@ -21,7 +59,16 @@ export default function App() {
 
       <SafeAreaProvider>
         <ThemeProvider>
-          <RestaurantsScreen />
+          <NavigationContainer>
+            <Tab.Navigator
+              initialRouteName='Restaurants'
+              screenOptions={tabScreenOptions}
+            >
+              <Tab.Screen name='Restaurants' component={RestaurantsScreen} />
+              <Tab.Screen name='Map' component={Map} />
+              <Tab.Screen name='Settings' component={Settings} />
+            </Tab.Navigator>
+          </NavigationContainer>
         </ThemeProvider>
       </SafeAreaProvider>
     </>
