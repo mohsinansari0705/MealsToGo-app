6bde5d4148b21929f9d3a293df4d95a95d7b0bd8
Mohsin Ansari <mohsinansari0705@gmail.com>
2026-04-02 01:12:33 +0530

    [client/common] LayoutContext created for BottomBar

---
+++ client/common/context/LayoutContext.ts
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export type BottomTabId = 'restaurants' | 'map' | 'settings';
+
+export class LayoutData {
+  bottomBarVisible = true;
+  bottomBarSelectedId = 'restaurants';
+  bottomBarHeight = 0;
+
+  setBottomBar(
+    old: LayoutData,
+    visible?: boolean,
+    selectedId?: BottomTabId,
+    height?: number
+  ) {
+    const next = new LayoutData();
+    console.log(`[bottombar] old layout context: ${old}`);
+
+    next.bottomBarVisible = visible ?? old.bottomBarVisible;
+    next.bottomBarSelectedId = selectedId ?? old.bottomBarSelectedId;
+    next.bottomBarHeight = height ?? old.bottomBarHeight;
+
+    return next;
+  }
+}
+
+type LayoutContextValue = {
+  layoutContext: LayoutData;
+  setLayoutContext: React.Dispatch<React.SetStateAction<LayoutData>>;
+};
+
+export const LayoutContext = React.createContext<LayoutContextValue>({
+  layoutContext: new LayoutData(),
+  setLayoutContext: () => {},
+});
