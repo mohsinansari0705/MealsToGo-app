3f418047c213fc5e1cfd25b6bf2a2da77399c6ba
Mohsin Ansari <mohsinansari0705@gmail.com>
2026-04-02 01:16:36 +0530

    [client/app] UPs in RestaurantScreen

--- client/app/restaurants/restaurants.screen.tsx
+++ client/app/restaurants/restaurants.screen.tsx
@@ -2,11 +2,11 @@ import React, { useState } from 'react';
 import { View, FlatList } from 'react-native';
 import { Searchbar } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { RestaurantInfoCard } from '../../components/RestaurantInfoCard';
+
 import { useTheme } from '../../theme/ThemeContext';
+import { RestaurantInfoCard } from '../../components/RestaurantInfoCard';
 
-
-export const RestaurantsScreen = () => {
+const RestaurantsScreen = () => {
   const [searchQuery, setSearchQuery] = useState<string>('');
   const theme = useTheme().theme;
 
@@ -21,11 +21,17 @@ export const RestaurantsScreen = () => {
       </View>
 
       <FlatList
-        data={[ {name: 1}, {name: 2}, {name: 3}, {name: 4}, {name: 5} ]}
+        data={[{ name: 1 }, { name: 2 }, { name: 3 }, { name: 4 }, { name: 5 }]}
         renderItem={() => <RestaurantInfoCard />}
         keyExtractor={(item) => item.name.toString()}
-        contentContainerStyle={{ paddingHorizontal: theme.space.md, gap: theme.space.md, marginBottom: theme.space.xl }}
+        contentContainerStyle={{
+          paddingHorizontal: theme.space.md,
+          gap: theme.space.md,
+          marginBottom: theme.space.xl,
+        }}
       />
     </SafeAreaView>
   );
 };
+
+export default RestaurantsScreen;
