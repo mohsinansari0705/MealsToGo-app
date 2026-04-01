ce758624e241962040dfbbbb45bc94064f7332df
Mohsin Ansari <mohsinansari0705@gmail.com>
2026-04-02 01:20:20 +0530

    [client/components] added new cover image & UPs

--- client/components/RestaurantInfoCard.tsx
+++ client/components/RestaurantInfoCard.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
 import { Card } from 'react-native-paper';
 import { SvgXml } from 'react-native-svg';
 import { Ionicons } from '@expo/vector-icons';
+import { View, Text, Image } from 'react-native';
+
 import open from '../assets/icons/open';
 import { useTheme } from '../theme/ThemeContext';
-
 
 type Restaurant = {
   name: string;
@@ -26,13 +26,14 @@ export const RestaurantInfoCard = () => {
     name: 'Some Restaurant',
     icon: 'https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png',
     photos: [
-      'https://imgs.search.brave.com/qLazr26Lu6vYYB-vHR6zUvWJh-KaXqczO7trkHnx8iY/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMudW5zcGxhc2gu/Y29tL3Bob3RvLTE2/MjU4MTM1MDYwNjIt/MGFlYjFkN2EwOTRi/P2ZtPWpwZyZxPTYw/Jnc9MzAwMCZhdXRv/PWZvcm1hdCZmaXQ9/Y3JvcCZpeGxpYj1y/Yi00LjEuMCZpeGlk/PU0zd3hNakEzZkRC/OE1IeHpaV0Z5WTJo/OE1URjhmR0oxY21k/bGNueGxibnd3Zkh3/d2ZIeDhNQT09',
+      'https://imgs.search.brave.com/zHpbo3QY1bMQdC-29Cc2OEH_0CeuFqa47KlSey4K8qk/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS1waG90/by9qdW5rLWZvb2Rf/MTI2MTgzOC01OTI4/LmpwZz9zZW10PWFp/c19oeWJyaWQmdz03/NDAmcT04MA',
+      'https://imgs.search.brave.com/Zt9SLvlHn25rgU0bwOgh8TTn3fdMowRv8LKQw0FBtyA/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTQy/ODQwOTUxNC9waG90/by9hLW1hbGUtY2hl/Zi1zZXJ2aW5nLWEt/ZmluZS1kaW5pbmct/ZGlzaC1pbi1hLXJl/c3RhdXJhbnQuanBn/P3M9NjEyeDYxMiZ3/PTAmaz0yMCZjPU9L/MGxBQ2JLdG82b2lP/amc1eGdVeFkyZlBT/OEN0VDNmWDlIaEsy/bnRRLU09',
     ],
     address: '100 some random street',
     isOpenNow: true,
     rating: 4.2,
     isClosedTemporarily: true,
-  }
+  };
 
   const theme = useTheme().theme;
 
@@ -45,10 +46,15 @@ export const RestaurantInfoCard = () => {
     // <View style={{ paddingHorizontal: theme.space.md }}>
     <View>
       <Card mode='contained'>
-        <Card.Cover
+        <Image
           key={restaurants.name}
-          source={{ uri: restaurants.photos[0] }}
-          style={{ margin: theme.space.md }}
+          source={{ uri: restaurants.photos[1] }}
+          resizeMode='stretch'
+          style={{
+            margin: theme.space.md,
+            height: theme.space.xxl * 4,
+            borderRadius: theme.space.sm,
+          }}
         />
         <Card.Content>
           <Text style={{ fontFamily: theme.fonts.heading, fontSize: theme.fontSize.md * 1.1 }}>
@@ -57,24 +63,35 @@ export const RestaurantInfoCard = () => {
 
           <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
             <View style={{ flexDirection: 'row', gap: theme.space.xs * 0.75, marginVertical: theme.space.xs }}>
-              {ratingArray.map((index) =>
+              {ratingArray.map((index) => (
                 <Ionicons
                   key={index}
                   name='star'
                   size={theme.space.md}
                   color={theme.colors.warning}
                 />
-              )}
+              ))}
             </View>
-            
+
             <View style={{ flexDirection: 'row', gap: theme.space.sm * 1.25, alignItems: 'center' }}>
               {restaurants.isClosedTemporarily && (
-                <Text style={{ fontFamily: theme.fonts.heading, color: theme.colors.danger, fontSize: theme.fontSize.sm }}>CLOSED TEMPORARILY</Text>
+                <Text
+                  style={{
+                    fontFamily: theme.fonts.heading,
+                    color: theme.colors.danger,
+                    fontSize: theme.fontSize.sm,
+                  }}
+                >CLOSED TEMPORARILY</Text>
               )}
 
-              {restaurants.isOpenNow && <SvgXml xml={open} width={theme.space.md * 1.25} height={theme.space.md * 1.25} />}
+              {restaurants.isOpenNow && (
+                <SvgXml xml={open} width={theme.space.md * 1.25} height={theme.space.md * 1.25} />
+              )}
 
-              <Image source={{ uri: restaurants.icon }} style={{ height: theme.space.md, width: theme.space.md }} />
+              <Image
+                source={{ uri: restaurants.icon }}
+                style={{ height: theme.space.md, width: theme.space.md }}
+              />
             </View>
           </View>
 
