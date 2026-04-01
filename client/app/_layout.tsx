98c52c3610c2f1caae0298ffc0392ad43ab2ffed
Mohsin Ansari <mohsinansari0705@gmail.com>
2026-04-02 01:15:16 +0530

    [client/app] shifted to expo-navigation and setup the _layout

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -1,56 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Slot } from 'expo-router';
+import { View } from 'react-native';
 import { useFonts } from 'expo-font';
-import { View, Text } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { Ionicons } from '@expo/vector-icons';
-import { ThemeProvider } from './theme/ThemeContext';
-import { NavigationContainer } from '@react-navigation/native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
-import { RestaurantsScreen } from './app/restaurants/restaurants.screen';
-
-
-const Tab = createBottomTabNavigator();
-const TAB_ICON: Record<string, keyof typeof Ionicons.glyphMap> = {
-  Restaurants: 'restaurant',
-  Map: 'map-sharp',
-  Settings: 'settings',
-};
-
-const Map = () => (
-  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text>Map!</Text>
-  </View>
-);
-const Settings = () => (
-  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text>Settings!</Text>
-  </View>
-);
-
-const tabScreenOptions = ({ route }: { route: any }) => {
-  let iconName = TAB_ICON[route.name];
-
-  return {
-    headerTransparent: true,
-    headerTitle: '',
-    tabBarIcon: ({ color, size }: { color: string; size: number }) => (
-      <Ionicons name={iconName} size={size} color={color} />
-    ),
-    tabBarActiveTintColor: 'tomato',
-    tabBarInactiveTintColor: 'gray',
-  };
-};
+import { ThemeProvider } from '../theme/ThemeContext';
+import { AppBottomBar } from '../components/common/AppBottomBar';
+import { LayoutContext, LayoutData } from '../common/context/LayoutContext';
 
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    Lato_400Regular: require('./assets/fonts/Lato-Regular.ttf'),
-    Oswald_400Regular: require('./assets/fonts/Oswald-Regular.ttf'),
-    Ubuntu_400Regular: require('./assets/fonts/Ubuntu-Regular.ttf'),
-  });
+  const [layoutContext, setLayoutContext] = useState(new LayoutData());
 
+  const [fontsLoaded] = useFonts({
+    Lato_400Regular: require('../assets/fonts/Lato-Regular.ttf'),
+    Oswald_400Regular: require('../assets/fonts/Oswald-Regular.ttf'),
+    Ubuntu_400Regular: require('../assets/fonts/Ubuntu-Regular.ttf'),
+  });
   if (!fontsLoaded) return null;
 
   return (
@@ -59,16 +26,22 @@ export default function App() {
 
       <SafeAreaProvider>
         <ThemeProvider>
-          <NavigationContainer>
-            <Tab.Navigator
-              initialRouteName='Restaurants'
-              screenOptions={tabScreenOptions}
-            >
-              <Tab.Screen name='Restaurants' component={RestaurantsScreen} />
-              <Tab.Screen name='Map' component={Map} />
-              <Tab.Screen name='Settings' component={Settings} />
-            </Tab.Navigator>
-          </NavigationContainer>
+          <LayoutContext.Provider value={{ layoutContext, setLayoutContext }}>
+            <SafeAreaView style={{ flex: 1 }}>
+              <View
+                style={{
+                  flex: 1,
+                  paddingBottom: layoutContext.bottomBarVisible
+                    ? layoutContext.bottomBarHeight
+                    : 0,
+                }}
+              >
+                <Slot />
+              </View>
+
+              <AppBottomBar basePath='/' />
+            </SafeAreaView>
+          </LayoutContext.Provider>
         </ThemeProvider>
       </SafeAreaProvider>
     </>
