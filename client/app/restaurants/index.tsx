b458f82bd883870b69e1dbb671d221ff73719182
Mohsin Ansari <mohsinansari0705@gmail.com>
2026-04-05 00:33:32 +0530

    [client/app] fetching Restaurants data from api and added loader

--- client/app/restaurants/index.tsx
+++ client/app/restaurants/index.tsx
@@ -1,15 +1,53 @@
-import React, { useState } from 'react';
-import { View, FlatList } from 'react-native';
+import React, { useState, useEffect, useContext } from 'react';
+import { View, FlatList, ActivityIndicator } from 'react-native';
 import { Searchbar } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { useTheme } from '../../theme/ThemeContext';
+import { AppContext } from '@/common/context/AppContext';
+import { getApiErrorMessage } from '@/common/api/api.errors';
 import { RestaurantInfoCard } from '../../components/RestaurantInfoCard';
 
 
 export default function RestaurantsScreen() {
-  const [searchQuery, setSearchQuery] = useState<string>('');
+  const appContext = useContext(AppContext);
   const theme = useTheme().theme;
+
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+  const [restaurants, setRestaurants] = useState<any[]>([]);
+  const [searchQuery, setSearchQuery] = useState<string>('');
+
+  const fetchRestaurantsData = async () => {
+    setLoading(true);
+    try {
+      const api = appContext.context.api;
+      const [restaurantsData] = await Promise.all([
+        api.getRestaurants(),
+      ]);
+      setRestaurants(restaurantsData);
+
+      setLoading(false);
+    } catch (err) {
+      setError(getApiErrorMessage(err));
+      console.log('Error fetching restaurants data:', error);
+
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchRestaurantsData();
+  }, []);
+
+  if (loading) {
+    return (
+      <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <ActivityIndicator size={theme.space.xl * 1.25} />
+      </SafeAreaView>
+    );
+  }
+
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
@@ -22,9 +60,9 @@ export default function RestaurantsScreen() {
       </View>
 
       <FlatList
-        data={[{ name: 1 }, { name: 2 }, { name: 3 }, { name: 4 }, { name: 5 }]}
-        renderItem={() => <RestaurantInfoCard />}
-        keyExtractor={(item) => item.name.toString()}
+        data={restaurants}
+        renderItem={({ item }) => <RestaurantInfoCard restaurant={item} />}
+        keyExtractor={(item) => item.id}
         contentContainerStyle={{
           paddingHorizontal: theme.space.md,
           gap: theme.space.md,
@@ -33,4 +71,4 @@ export default function RestaurantsScreen() {
       />
     </SafeAreaView>
   );
-};
+}
