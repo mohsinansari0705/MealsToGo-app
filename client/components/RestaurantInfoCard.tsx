f0c1ab53a9831a1bf3ca1ed05d54322d85a0c906
Mohsin Ansari <mohsinansari0705@gmail.com>
2026-04-05 00:29:42 +0530

    [client/components] rendering destructured Restaurant data from parent

--- client/components/RestaurantInfoCard.tsx
+++ client/components/RestaurantInfoCard.tsx
@@ -6,49 +6,40 @@ import { View, Text, Image } from 'react-native';
 
 import open from '../assets/icons/open';
 import { useTheme } from '../theme/ThemeContext';
+import type { Restaurant } from '../common/types/types';
 
-type Restaurant = {
-  name: string;
-  icon: string;
-  photos: string[];
-  address: string;
-  isOpenNow: boolean;
-  rating: number;
-  isClosedTemporarily: boolean;
+
+type RestaurantInfoCardProps = {
+  restaurant: Restaurant;
 };
-// type RestaurantInfoCardProps = {
-//   restaurants: Restaurant;
-// };
 
-// export const RestaurantInfoCard = ({ restaurants }: RestaurantInfoCardProps) => {
-export const RestaurantInfoCard = () => {
-  const restaurants: Restaurant = {
-    name: 'Some Restaurant',
-    icon: 'https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png',
-    photos: [
-      'https://imgs.search.brave.com/zHpbo3QY1bMQdC-29Cc2OEH_0CeuFqa47KlSey4K8qk/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS1waG90/by9qdW5rLWZvb2Rf/MTI2MTgzOC01OTI4/LmpwZz9zZW10PWFp/c19oeWJyaWQmdz03/NDAmcT04MA',
-      'https://imgs.search.brave.com/Zt9SLvlHn25rgU0bwOgh8TTn3fdMowRv8LKQw0FBtyA/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTQy/ODQwOTUxNC9waG90/by9hLW1hbGUtY2hl/Zi1zZXJ2aW5nLWEt/ZmluZS1kaW5pbmct/ZGlzaC1pbi1hLXJl/c3RhdXJhbnQuanBn/P3M9NjEyeDYxMiZ3/PTAmaz0yMCZjPU9L/MGxBQ2JLdG82b2lP/amc1eGdVeFkyZlBT/OEN0VDNmWDlIaEsy/bnRRLU09',
-    ],
-    address: '100 some random street',
-    isOpenNow: true,
-    rating: 4.2,
-    isClosedTemporarily: true,
-  };
-
+export const RestaurantInfoCard = ({ restaurant }: RestaurantInfoCardProps) => {
   const theme = useTheme().theme;
 
-  const ratingArray: any[] = Array.from(
-    new Array(Math.floor(restaurants.rating)),
+  const {
+    id,
+    name,
+    address,
+    photos,
+    icon,
+    rating,
+    userRatingsTotal,
+    isOpenNow,
+    isClosedTemporarily,
+  } = restaurant;
+
+  const ratingArray: number[] = Array.from(
+    new Array(Math.floor(rating)),
     (_, i) => i
   );
 
+  
   return (
-    // <View style={{ paddingHorizontal: theme.space.md }}>
     <View>
       <Card mode='contained'>
         <Image
-          key={restaurants.name}
-          source={{ uri: restaurants.photos[1] }}
+          key={id}
+          source={{ uri: photos[0] }}
           resizeMode='stretch'
           style={{
             margin: theme.space.md,
@@ -58,45 +49,44 @@ export const RestaurantInfoCard = () => {
         />
         <Card.Content>
           <Text style={{ fontFamily: theme.fonts.heading, fontSize: theme.fontSize.md * 1.1 }}>
-            {restaurants.name}
+            {name}
           </Text>
 
           <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-            <View style={{ flexDirection: 'row', gap: theme.space.xs * 0.75, marginVertical: theme.space.xs }}>
-              {ratingArray.map((index) => (
-                <Ionicons
+            <View style={{ flexDirection: 'row', gap: theme.space.sm, alignItems: 'center' }}>
+              <View style={{ flexDirection: 'row', gap: theme.space.xs * 0.75, marginVertical: theme.space.xs }}>
+                {ratingArray.map((index) => (
+                  <Ionicons
                   key={index}
                   name='star'
                   size={theme.space.md}
                   color={theme.colors.warning}
-                />
-              ))}
+                  />
+                ))}
+              </View>
+              <Text>({userRatingsTotal})</Text>
             </View>
 
             <View style={{ flexDirection: 'row', gap: theme.space.sm * 1.25, alignItems: 'center' }}>
-              {restaurants.isClosedTemporarily && (
-                <Text
-                  style={{
-                    fontFamily: theme.fonts.heading,
-                    color: theme.colors.danger,
-                    fontSize: theme.fontSize.sm,
-                  }}
-                >CLOSED TEMPORARILY</Text>
+              {isClosedTemporarily && (
+                <Text style={{ fontFamily: theme.fonts.heading, color: theme.colors.danger, fontSize: theme.fontSize.sm }}>
+                  CLOSED TEMPORARILY
+                </Text>
               )}
 
-              {restaurants.isOpenNow && (
+              {isOpenNow && (
                 <SvgXml xml={open} width={theme.space.md * 1.25} height={theme.space.md * 1.25} />
               )}
 
               <Image
-                source={{ uri: restaurants.icon }}
+                source={{ uri: icon }}
                 style={{ height: theme.space.md, width: theme.space.md }}
               />
             </View>
           </View>
 
           <Text style={{ fontFamily: theme.fonts.body, fontSize: theme.fontSize.md }}>
-            {restaurants.address}
+            {address}
           </Text>
         </Card.Content>
       </Card>
