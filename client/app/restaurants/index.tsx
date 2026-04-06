a0779fec420c54333bdc86b031d8e03bebb8afc3
Mohsin Ansari <mohsinansari0705@gmail.com>
2026-04-07 01:27:22 +0530

    [client/app] added location context to render data from that locaion Coords

--- client/app/restaurants/index.tsx
+++ client/app/restaurants/index.tsx
@@ -1,49 +1,75 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { View, FlatList, ActivityIndicator } from 'react-native';
-import { Searchbar } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { View, Text, FlatList, ActivityIndicator } from 'react-native';
 
+import { Restaurant } from '@/common/types/types';
 import { useTheme } from '../../theme/ThemeContext';
 import { AppContext } from '@/common/context/AppContext';
 import { getApiErrorMessage } from '@/common/api/api.errors';
+import { LocationContext } from '@/common/context/LocationContext';
 import { RestaurantInfoCard } from '../../components/RestaurantInfoCard';
+import { SearchBar } from '../../components/restaurants/SearchComponent';
 
 
 export default function RestaurantsScreen() {
-  const appContext = useContext(AppContext);
   const theme = useTheme().theme;
+  const appContext = useContext(AppContext);
+  const locationContext = useContext(LocationContext);
 
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
-  const [restaurants, setRestaurants] = useState<any[]>([]);
-  const [searchQuery, setSearchQuery] = useState<string>('');
+  const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
 
-  const fetchRestaurantsData = async () => {
+  const fetchRestaurantsData = async (lat: number, lng: number) => {
+    setError(null);
     setLoading(true);
+    setRestaurants([]);
+
     try {
       const api = appContext.context.api;
+      
       const [restaurantsData] = await Promise.all([
-        api.getRestaurants(),
+        api.getRestaurants(lat, lng),
       ]);
       setRestaurants(restaurantsData);
 
       setLoading(false);
     } catch (err) {
       setError(getApiErrorMessage(err));
-      console.log('Error fetching restaurants data:', error);
-
       setLoading(false);
     }
   };
 
   useEffect(() => {
-    fetchRestaurantsData();
-  }, []);
+    if (!locationContext.location) {
+      return;
+    } else {
+      const { lat, lng } = locationContext.location;
+      fetchRestaurantsData(lat, lng);
+    }
 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [locationContext.location]);
+
+  if (error) {
+    return (
+      <SafeAreaView style={{ flex: 1 }}>
+        <SearchBar />
+        <View style={{ flex: 1, paddingHorizontal: theme.space.xxl * 2, justifyContent: 'center' }}>
+          <Text style={{ textAlign: 'center' }}>
+            {error}
+          </Text>
+        </View>
+      </SafeAreaView>
+    );
+  }
   if (loading) {
     return (
-      <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <SearchBar />
+        <View style={{ flex: 1, paddingHorizontal: theme.space.xxl * 2, justifyContent: 'center' }}>
           <ActivityIndicator size={theme.space.xl * 1.25} />
+        </View>
       </SafeAreaView>
     );
   }
@@ -51,13 +77,7 @@ export default function RestaurantsScreen() {
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <View style={{ paddingHorizontal: theme.space.md, paddingVertical: theme.space.sm * 1.5 }}>
-        <Searchbar
-          placeholder='Search'
-          onChangeText={setSearchQuery}
-          value={searchQuery}
-        />
-      </View>
+      <SearchBar />
 
       <FlatList
         data={restaurants}
